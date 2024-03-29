e69aabdf20efed8c76af9d4e37ed6c6c75bc98c1
MercyAkpan <akpanmeme@gmail.com>
2024-03-29 15:15:22 +0100

    Alx

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,11 +1,10 @@
 #!/usr/bin/env ruby
-
-as = /(School)?/
-string = ARGV
-string.each do |arg|
-  puts "#{arg}"
-#puts "#{as}"
-  if arg =~ as  # Replace with your desired pattern
-    print "true\n"
-  end
+#MINE
+as = /School/
+var = ARGV.join("")
+#This joins all C-L args into a string (var).ARGV is an array of strings. Var is of
+#type <string>
+var.scan(as) do |match| #.scan is a string method to scan for non-overlapping matches substrings
+						#then each substring is held by the 'match' variable  
+  print match #prints each match
 end
