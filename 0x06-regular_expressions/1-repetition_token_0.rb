f2bca1559f69ec707c3ae742bd143a0f66e01294
MercyAkpan <akpanmeme@gmail.com>
2024-03-29 16:12:01 +0100

    Alx

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/env ruby
 #MINE
-as = /hb(t){2,5}n/
+as = /(hbt{2,5}n)/
 var = ARGV.join("")
+#puts (var)
 #puts (var)
 #This joins all C-L args into a string (var).ARGV is an array of strings. Var is of
 #type <string>
 var.scan(as) do |match| #.scan is a string method to scan for non-overlapping matches substrings
                                                 #then each substring is held by the 'match' variable
-  print match #prints each match
+  puts match #prints each match
 end
