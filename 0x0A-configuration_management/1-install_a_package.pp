b5dd4246de3d8e57f07a7596348e62ec804b38d3
MercyAkpan <akpanmeme@gmail.com>
2024-04-22 12:19:08 +0100

    alx

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,7 +3,7 @@ package { 'Werkzeug': # package to be installed
   provider => 'pip3', #provider(package manager) to install package
 }
 #
-package { 'python3-flask': # package to be installed
+package { 'flask': # package to be installed
   ensure   => '2.1.0', # 'installed'== latest version, 'version_num'=> installs particular version
   provider => 'pip3', #provider(package manager) to install package
 }
