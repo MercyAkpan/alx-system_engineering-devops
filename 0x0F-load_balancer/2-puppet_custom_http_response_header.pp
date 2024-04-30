0ee01a533873ed89359d1cc50619fab1db83530b
MercyAkpan <akpanmeme@gmail.com>
2024-04-30 01:56:38 +0100

    alx

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,32 +1,23 @@
-#MINE
-$package = 'Nginx HTTP'
-$text = 'Hello World!'
-#$new_string = 'server_name _;\n\tlocation /redirect_me {\n\t\trewrite ^/redirect_me https://www.google.com permanent;\n\t}'
-#$new_string = 'server_name _;\n\tlocation /redirect_me {\n\t\treturn 301 https://www.mountainoffire.org ;\n\t}'
-exec { 'install_nginx':
-  command => 'sudo apt install -y nginx',
-  unless  => 'dpkg -l nginx',
-  path    => ['/bin', '/usr/bin'],
-}
-exec { 'allow_nginx_traffic':
-  command => "sudo ufw allow \"$package\"",
-#  command => "sudo ufw allow \"$package\"",
-  path    => ['/bin', '/usr/bin'],
+# Update package information
+exec { 'apt-update':
+  command => '/usr/bin/apt-get -y update',
+  path    => ['/usr/bin', '/bin'],
 }
 
-exec { 'set_up_nginx_content':
-  command => "echo $text | sudo tee /var/www/html/index.html",
-  path    => ['/bin', '/usr/bin'],
+# Install Nginx package
+package { 'nginx':
+  ensure => installed,
 }
 
-exec { 'set_up_nginxcontent':
-#  command => "sudo sed -i \"s#server {#server {\n\tadd_header X-Served-By \$hostname;#\" ~/2-ver",
-  command     => "sudo sed -i '/server {/a \\\tadd_header X-Served-By \$hostname;' /etc/nginx/sites-enabled/default",
-  path    => ['/bin', '/usr/bin'],
+# Configure Nginx to add the custom HTTP header
+file_line { 'add custom header':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "\tadd_header X-Served-By ${hostname};",
+  after  => 'server_name _;',
 }
 
-
-exec { 'restart_nginx':
-  command => 'sudo service nginx restart',
-  path    => ['/bin', '/usr/bin'],
+# Ensure Nginx is running
+service { 'nginx':
+  ensure => running,
 }
