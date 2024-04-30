c498c951746b08420972d2d9f773e31d3bbf0742
MercyAkpan <akpanmeme@gmail.com>
2024-04-30 16:45:27 +0100

    alx

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,24 +1,24 @@
 #MINE
 $package = 'Nginx HTTP'
 $text = 'Hello World!'
-$new_string = 'server_name _;\n\tlocation /redirect_me {\n\t\trewrite ^/redirect_me https://www.google.com permanent;\n\t}'
+#$new_string = 'server_name _;\n\tlocation /redirect_me {\n\t\trewrite ^/redirect_me https://www.google.com permanent;\n\t}'
 exec { 'install_nginx':
   command => 'sudo apt install -y nginx',
   unless  => 'dpkg -l nginx',
   path    => ['/bin', '/usr/bin'],
 }
 exec { 'allow_nginx_traffic':
-  command => "sudo ufw allow \"$package\"",
+  command => "sudo ufw allow ${package}",
   path    => ['/bin', '/usr/bin'],
 }
 
 exec { 'set_up_landing_content':
-  command => "echo $text | sudo tee /var/www/html/index.html",
+  command => "echo ${text} | sudo tee /var/www/html/index.html",
   path    => ['/bin', '/usr/bin'],
 }
 
 exec { 'redirect':
-  command => "sudo sed -i \"s#server_name _;#$new_string#\"  /etc/nginx/sites-enabled/default",
+  command => "sudo sed -i \"s#server_name _;#'server_name _;\n\tlocation /redirect_me {\n\t\trewrite ^/redirect_me https://www.google.com permanent;\n\t}'#\"  /etc/nginx/sites-enabled/default",
   path    => ['/bin', '/usr/bin'],
 }
 exec { 'adding_headers':
