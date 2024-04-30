c861e0e0378bd985b9ff7f03edd493568653b375
MercyAkpan <akpanmeme@gmail.com>
2024-04-30 14:02:36 +0100

    alx

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,8 @@
 #MINE
 $package = 'Nginx HTTP'
 $text = 'Hello World!'
-#$new_string = 'server_name _;\n\tlocation /redirect_me {\n\t\trewrite ^/redirect_me https://www.google.com permanent;\n\t}'
-$new_string = 'server_name _;\n\tlocation /redirect_me {\n\t\treturn 301 https://www.mountainoffire.org ;\n\t}'
+$new_string = 'server_name _;\n\tlocation /redirect_me {\n\t\trewrite ^/redirect_me https://www.google.com permanent;\n\t}'
+#$new_string = 'server_name _;\n\tlocation /redirect_me {\n\t\treturn 301 https://www.mountainoffire.org ;\n\t}'
 exec { 'install_nginx':
   command => 'sudo apt install -y nginx',
   unless  => 'dpkg -l nginx',
@@ -14,17 +14,21 @@ exec { 'allow_nginx_traffic':
   path    => ['/bin', '/usr/bin'],
 }
 
-exec { 'set_up_nginx_content':
+exec { 'set_up_landing_content':
   command => "echo $text | sudo tee /var/www/html/index.html",
   path    => ['/bin', '/usr/bin'],
 }
 
+exec { 'redirect':
+#  command => "sudo sed -i \"s#server {#server {\n\tadd_header X-Served-By \$hostname;#\" ~/2-ver",
+  command => "sudo sed -i \"s#server_name _;#$new_string#\"  /etc/nginx/sites-enabled/default"   
+  path    => ['/bin', '/usr/bin'],
+}        
 exec { 'set_up_nginxcontent':
 #  command => "sudo sed -i \"s#server {#server {\n\tadd_header X-Served-By \$hostname;#\" ~/2-ver",
   command     => "sudo sed -i '/server {/a \\\tadd_header X-Served-By \$hostname;' /etc/nginx/sites-enabled/default",
   path    => ['/bin', '/usr/bin'],
 }
-
 
 exec { 'restart_nginx':
   command => 'sudo service nginx restart',
