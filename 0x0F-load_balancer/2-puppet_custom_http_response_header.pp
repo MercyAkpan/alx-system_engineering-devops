ee563ab61534d6a99edb53c10fe7b14ed67ce4a7
MercyAkpan <akpanmeme@gmail.com>
2024-05-01 01:29:55 +0100

    alx

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,32 +1,23 @@
-#MINE
-$package = 'Nginx HTTP'
-$text = 'Hello World!'
-#$new_string = 'server_name _;\n\tlocation /redirect_me {\n\t\trewrite ^/redirect_me https://www.google.com permanent;\n\t}'
-exec { 'install_nginx':
-  command => 'sudo apt install -y nginx',
-  unless  => 'dpkg -l nginx',
-  path    => ['/bin', '/usr/bin'],
-}
-exec { 'allow_nginx_traffic':
-  command => "sudo ufw allow ${package}",
-  path    => ['/bin', '/usr/bin'],
+# Update package information
+exec { 'apt-update':
+  command => '/usr/bin/apt-get -y update',
+  path    => ['/usr/bin', '/bin'],
 }
 
-exec { 'set_up_landing_content':
-  command => "echo ${text} | sudo tee /var/www/html/index.html",
-  path    => ['/bin', '/usr/bin'],
+# Install Nginx package
+package { 'nginx':
+  ensure => installed,
 }
 
-exec { 'redirect':
-  command => "sudo sed -i \"s#server_name _;#'server_name _;\n\tlocation /redirect_me {\n\t\trewrite ^/redirect_me https://www.google.com permanent;\n\t}'#\"  /etc/nginx/sites-enabled/default",
-  path    => ['/bin', '/usr/bin'],
-}
-exec { 'adding_headers':
-  command => "sudo sed -i '/server {/a \\\tadd_header X-Served-By \$hostname;' /etc/nginx/sites-enabled/default",
-  path    => ['/bin', '/usr/bin'],
+# Configure Nginx to add the custom HTTP header
+file_line { 'add custom header':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "\tadd_header X-Served-By ${hostname};",
+  after  => 'server_name _;',
 }
 
-exec { 'restart_nginx':
-  command => 'sudo service nginx restart',
-  path    => ['/bin', '/usr/bin'],
+# Ensure Nginx is running
+service { 'nginx':
+  ensure => running,
 }
