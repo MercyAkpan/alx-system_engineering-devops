ce50bbb2c309f911cc0c48dc55b3544da1af1c75
MercyAkpan <akpanmeme@gmail.com>
2024-05-01 14:26:00 +0100

    Alx:

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,34 +1,16 @@
 #MINE
-#exec { 'apt-update':
-#  command => '/usr/bin/apt-get -y update',
-#  path    => ['/usr/bin', '/bin'],
-#}
+exec { 'apt-update':
+  command => '/usr/bin/sudo apt-get -y update',
+  path    => ['/usr/bin', '/bin'],
+}
 
 exec { 'install_nginx':
   command => 'sudo apt install -y nginx',
-  unless  => 'dpkg -l nginx',
+  unless  => '/usr/bin/dpkg-query -l nginx ',
   path    => ['/bin', '/usr/bin'],
-}
-#exec { 'allow_nginx_traffic':
-#  command => "sudo ufw allow ${package}",
-#  path    => ['/bin', '/usr/bin'],
-#}
-
-#exec { 'set_up_landing_content':
-#  command => "echo ${text} | sudo tee /var/www/html/index.html",
-#  path    => ['/bin', '/usr/bin'],
-#}
-#
-#exec { 'redirect':
-#  command => "sudo sed -i \"s#server_name _;#'server_name _;\n\tlocation /redirect_me {\n\t\trewrite ^/redirect_me https://www.google.com permanent;\n\t}'#\"  /etc/nginx/sites-enabled/default",
-#  path    => ['/bin', '/usr/bin'],
-#}
-service { 'nginx':
-  ensure  => running,
 }
 
 exec { 'adding_headers':
-#  command => "sudo sed -i '/server {/a \\\tadd_header X-Served-By \$hostname;' /etc/nginx/sites-enabled/default",
   command => 'sudo sed -i "0,/server {/s#server {#server {\n\tadd_header X-Served-By \$hostname;#" /etc/nginx/sites-enabled/default',
   path    => ['/bin', '/usr/bin'],
 }
