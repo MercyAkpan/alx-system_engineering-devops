3c8798220a2367fd3c4fcabcb0f9863a577d2271
MercyAkpan <akpanmeme@gmail.com>
2024-06-01 18:41:17 +0100

    alx

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -3,7 +3,7 @@
     This script extracts user data and to-do task data from two web APIs :
     https://jsonplaceholder.typicode.com/users/
     https://jsonplaceholder.typicode.com/todos/,
-    and sends it, into csv format, same format used in EXCEL spreadsheet.
+    and export it as JSON format.
     for a specific user.
 """
 import json
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         user_id (str): The ID of the user for whom to retrieve data.
 
         Returns:
-        A csv file with all tasks assigned to user,(using username)
+        A JSON file with all tasks assigned to user,(using username)
         """
 
     try:
@@ -38,35 +38,35 @@ if __name__ == "__main__":
         todo_data = todo_response.json()
         # Note: tasks_user is a list of dictionaries
         tasks_user = [task for task in todo_data
-                       if task.get('userId') == int(user_id)]
-#        print(tasks_user[0])
-#        print()
+                      if task.get('userId') == int(user_id)]
         # and task.get('completed') is True] (if I want only completed tasks)
         # File to save it in.
         files = f"{user_id}.json"
+        # Reorder the keys of the dictionary
         ordered_keys = ["title", "completed", "username"]
         # Print the reordered dictionary
+        # Open a JSON file, new or existing.
         with open(files, 'w', newline='') as jsonfile:
-                # fieldnames (columns for each field, but it won't be printed out as a header)
-                    # allocating each fieldname to respective field.(unpacking the dictionary)
+            # Removing "id" key, adding "username" key
             key_to_remove = "id"
             new_key = "username"
             new_value = username
             for d in tasks_user:
-#                print(d)
-#                print("==============")
-            # Remove the key from the dictionary
-                d.pop(key_to_remove, None)  # None is the default value if the key does not exist
+                # Remove the key from the dictionary
+                d.pop(key_to_remove, None)  # None is the default value
+                #                           if the key does not exist
             # Add the new key-value pair to the dictionary
                 d[new_key] = new_value
-                reordered_items = [("task" if key == "title" else key, d[key]) for key in ordered_keys]
-#                reordered_items = [(key, d[key]) for key in ordered_keys if key == 'title', ]
+            # Makes a new list of tuples, with key,value pair in each tuple
+            # Changes the "title" key to "task", and re-order keys
+                reordered_items = [("task" if key == "title" else key,
+                                    d[key]) for key in ordered_keys]
+            # Clear old dictionary
                 d.clear()
             # Update the dictionary with the reordered items
                 d.update(reordered_items)
 #                print(d) # How am i able to access the last 'd', out of scope.
-#                print("****************")
-            newdict = {user_id : tasks_user}
+            newdict = {user_id: tasks_user}
 #            print(newdict)
             json_string = json.dump(newdict, jsonfile)
     except Exception as e:
