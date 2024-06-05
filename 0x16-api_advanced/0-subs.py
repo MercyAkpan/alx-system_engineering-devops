20e2cbb4a1912ece9bb7dc41bd1cf84d4dd9c303
MercyAkpan <akpanmeme@gmail.com>
2024-06-05 01:46:05 +0100

    alx

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import requests
+
+
+def number_of_subscribers(subreddit):
+    try:
+        users_api_url = f"https://www.reddit.com/r/{subreddit}/about.json"
+        headers = {'User-Agent': 'MyRedditApp/1.0'}
+        # user_response always returns status code
+        user_response = requests.get(users_api_url, headers=headers)
+        user_response.raise_for_status()
+        user_data = user_response.json() # have it readable JSON format.
+        return user_data["data"]["subscribers"]
+    except requests.exceptions.HTTPError as errh:
+        if user_response.status_code == 404:
+            return 0
